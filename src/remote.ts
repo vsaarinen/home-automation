2643f6e13e40d55533ee3d5ead9059b72c775e0b
Ville Saarinen <ville.saarinen@gmail.com>
2016-11-27 23:29:51 +0200

    Add typings to Particle responses

--- src/remote.ts
+++ src/remote.ts
@@ -4,16 +4,46 @@ const particle = new Particle();
 let accessToken: string;
 let deviceId: string;
 
+interface ParticleResponse<T> {
+  body: T;
+  statusCode: number;
+}
+
+interface Login {
+  token_type: 'bearer';
+  access_token: string;
+  expires_in: number;
+  refresh_token: string;
+};
+
+interface Device {
+  id: string;
+  name: string;
+  last_app?: string;
+  last_ip_address?: string;
+  last_heard?: string;
+  product_id: number;
+  connected: boolean;
+  platform_id: number;
+  cellular: boolean;
+  status: string;
+}
+
+interface FunctionCall {
+  id: string;
+  last_app: string;
+  connected: boolean;
+  return_value: number;
+}
+
 const loginPromise = particle.login({
   username: process.env['PARTICLE_EMAIL'],
   password: process.env['PARTICLE_PASSWORD'],
-})
-.then((data: any) => {
+}).then((data: ParticleResponse<Login>) => {
   console.log('Logged in to Particle successfully');
   accessToken = data.body.access_token;
   return particle.listDevices({ auth: accessToken });
-})
-.then((devices: any) => {
+}).then((devices: ParticleResponse<Device[]>) => {
   deviceId = devices.body[0].id;
 }).catch((err: any) => {
   console.log('Particle initialization failed:', err);
@@ -30,7 +60,7 @@ const callParticleFunction = (command: string, group: string) => {
       name: command,
       argument: group,
       auth: accessToken,
-    }).then((data: any) => {
+    }).then((data: ParticleResponse<FunctionCall>) => {
       console.log(`Tried to ${command} group ${group}. ` +
         `Connected: ${data.body.connected}, result: ${data.body.return_value}`);
     }).catch((err: any) => {
