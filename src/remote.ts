7b3ad39fc522500bf217a0eaa41e7ca6212cbbd1
Ville Saarinen <ville.saarinen@gmail.com>
2017-09-21 22:56:09 +0300

    Switch from Particle to Telldus

--- src/remote.ts
+++ src/remote.ts
@@ -1,43 +1,58 @@
 import { error, log } from './log';
 import { storeAction } from './permanent-store';
 
-const Particle = require('particle-api-js');
-
-const particle = new Particle();
-let accessToken: string;
-let deviceId: string;
-
-interface ParticleResponse<T> {
-  body: T;
-  statusCode: number;
-}
-
-interface Login {
-  token_type: 'bearer';
-  access_token: string;
-  expires_in: number;
-  refresh_token: string;
-}
+const TelldusAPI = require('telldus-live');
 
 interface Device {
   id: string;
+  clientDeviceId: string;
   name: string;
-  last_app?: string;
-  last_ip_address?: string;
-  last_heard?: string;
-  product_id: number;
-  connected: boolean;
-  platform_id: number;
-  cellular: boolean;
-  status: string;
+  state: number;
+  statevalue: null | string;
+  methods: number;
+  type: 'group' | 'device';
+  client: string;
+  clientName: string;
+  online: string;
+  editable: number;
+  ignored: number;
+  devices?: string;
+  status: 'off' | 'on';
 }
 
-interface FunctionCall {
-  id: string;
-  last_app: string;
-  connected: boolean;
-  return_value: number;
-}
+const publicKey = process.env.TELLDUS_PUBLIC_KEY;
+const privateKey = process.env.TELLDUS_PRIVATE_KEY;
+const token = process.env.TELLDUS_TOKEN;
+const tokenSecret = process.env.TELLDUS_TOKEN_SECRET;
+let devices: Device[];
+let speaker: Device | undefined;
+let livingRoom: Device | undefined;
+let diningRoom: Device | undefined;
+let allLights: Device | undefined;
+
+const cloud = new TelldusAPI.TelldusAPI({ publicKey, privateKey })
+  .login(token, tokenSecret, (err: any, _user: any) => {
+    if (!!err) {
+      return error('login error: ' + err.message);
+    }
+
+    log('[telldus] Logged in to Telldus successfully');
+  })
+  .on('error', (err: any) => {
+    error('background error: ' + err.message);
+  });
+
+cloud.getDevices((err: any, devicesResponse: any) => {
+  if (!!err) {
+    return error('getDevices error: ' + err.message);
+  }
+
+  devices = devicesResponse;
+  speaker = devices.find(d => d.name === 'Speakers');
+  livingRoom = devices.find(d => d.name === 'Living room');
+  diningRoom = devices.find(d => d.name === 'Dining room');
+  allLights = devices.find(d => d.name === 'Lights');
+});
 
 export enum AutomationActionCommand {
   DISABLE_LIGHT,
@@ -50,80 +65,54 @@ export interface AutomationAction {
   manual?: boolean;
 }
 
-const loginPromise = particle.login({
-  username: process.env.PARTICLE_EMAIL,
-  password: process.env.PARTICLE_PASSWORD,
-}).then((data: ParticleResponse<Login>) => {
-  log('[particle] Logged in to Particle successfully');
-  accessToken = data.body.access_token;
-  return particle.listDevices({ auth: accessToken });
-}).then((devices: ParticleResponse<Device[]>) => {
-  deviceId = devices.body[0].id;
-}).catch((err: any) => {
-  error('[particle] Particle initialization failed:', err);
-});
-
-const callParticleFunction = (command: string, group: string): Promise<void> =>
-  loginPromise.then(() => {
-    if (!accessToken || !deviceId) {
-      throw new Error('Particle connection not initialized!');
-    }
-
-    log(`[particle] Sending ${command} to group ${group}.`);
-    particle.callFunction({
-      deviceId,
-      name: command,
-      argument: group,
-      auth: accessToken,
-    }).then((data: ParticleResponse<FunctionCall>) => {
-      log(`[particle] Result for ${command} @ ${group}: ${data.body.return_value}`);
-    }).catch((err: any) => {
-      error(`[particle] Unable to ${command} group ${group}:`, err);
-    });
-  });
-
-// Takes the desired actions sequentially and stores them to the permanent storage
+// Takes the desired actions and stores them to the permanent storage
 export const takeActions = (actions: AutomationAction[]) => {
-  let p: Promise<{} | void> = Promise.resolve();
-  actions.forEach(action => {
-    p = p.then(() => handleAction(action));
-  });
-  return p;
-};
-
-let activeCommands: Promise<any>[] = [];
-
-const handleAction = (action: AutomationAction) => {
-  let functionToCall: string;
-  log(`[action] Handling action ${action.command} on ${action.target}`);
-  switch (action.command) {
-    case AutomationActionCommand.ENABLE_LIGHT:
-      functionToCall = 'turnOn';
-      break;
-    case AutomationActionCommand.DISABLE_LIGHT:
-      functionToCall = 'turnOff';
-      break;
-    default:
-      throw new Error(`Unknown command ${action.command}`);
+  if (!devices) {
+    error('[telldus] No device information available');
+    return Promise.reject('Unabled to connect to Telldus');
   }
 
-  let actionPromise: Promise<any>;
-
-  // Only allow one remote action at a time. Thus we wait for any previous
-  // commands to finish.
-  return Promise.all(activeCommands)
-    .then(() => {
-      actionPromise = new Promise((resolve, _reject) =>
-        // We need to wait for at least 2 seconds when enabling/disabling lights
-        // because that's how long the button press is simulated for on the remote.
-        callParticleFunction(functionToCall, action.target)
-          .then(() => { setTimeout(() => resolve(action), 2500); }),
-      );
-      activeCommands.push(actionPromise);
-      return actionPromise;
-    })
-    .then(() => {
-      activeCommands = activeCommands.filter(command => command !== actionPromise);
-      return storeAction(action);
-    });
+  return Promise.all(
+    actions.map(action => new Promise(
+      (resolve, reject) => handleAction(action, resolve, reject),
+    )));
 };
+
+function handleAction(
+  action: AutomationAction,
+  resolve: (value?: any | PromiseLike<any>) => void,
+  reject: (reason?: any) => void,
+) {
+  let targetDevice: Device | undefined;
+  log(`[action] Handling action ${action.command} on ${action.target}`);
+
+  switch (action.target) {
+    case '2':
+      targetDevice = livingRoom;
+      break;
+    case '3':
+      targetDevice = speaker;
+      break;
+    case '1':
+      targetDevice = diningRoom;
+      break;
+  }
+
+  if (!targetDevice) {
+    error('[action] Unknonwn target', action.target);
+    return reject('Unknown target');
+  }
+
+  cloud.onOffDevice(
+    targetDevice,
+    action.command === AutomationActionCommand.ENABLE_LIGHT,
+    (err: any, _result: any) => {
+      if (!!err) {
+        error('[telldus] onOffDevice error: ' + err.message);
+        reject('Unable to send on/off command');
+      }
+
+      return storeAction(action).then(() => resolve());
+    },
+  );
+}
