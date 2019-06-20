37728a8f4d2e40a612bc916a82efb6f3f3c5db21
Ville Saarinen <ville.saarinen@gmail.com>
2019-06-20 20:26:19 +0300

    Enable automatic night light control

--- src/time.ts
+++ src/time.ts
@@ -6,104 +6,149 @@ import { Store } from 'redux';
 import { lightSet } from './actions';
 import { Device } from './devices';
 import { error } from './log';
-import {
-  AutomationAction,
-  AutomationActionCommand,
-  takeActions,
-} from './remote';
+import { AutomationActionCommand, takeActions } from './remote';
 
-interface SunCalcObject {
-  sunrise: Date;
-  sunriseEnd: Date;
-  goldenHourEnd: Date;
-  solarNoon: Date;
-  goldenHour: Date;
-  sunsetStart: Date;
-  sunset: Date;
-  dusk: Date;
-  nauticalDusk: Date;
-  night: Date;
-  nadir: Date;
-  nightEnd: Date;
-  nauticalDawn: Date;
-  dawn: Date;
-}
+// interface SunCalcObject {
+//   sunrise: Date;
+//   sunriseEnd: Date;
+//   goldenHourEnd: Date;
+//   solarNoon: Date;
+//   goldenHour: Date;
+//   sunsetStart: Date;
+//   sunset: Date;
+//   dusk: Date;
+//   nauticalDusk: Date;
+//   night: Date;
+//   nadir: Date;
+//   nightEnd: Date;
+//   nauticalDawn: Date;
+//   dawn: Date;
+// }
 
-interface SunCalc {
-  getTimes(date: Date, latitude: number, longtitude: number): SunCalcObject;
-}
-const suncalc: SunCalc = require('suncalc');
+// interface SunCalc {
+//   getTimes(date: Date, latitude: number, longtitude: number): SunCalcObject;
+// }
+// const suncalc: SunCalc = require('suncalc');
 
-if (!process.env.LATITUDE || !process.env.LONGTITUDE) {
-  throw new Error(
-    'LATITUDE and LONGTITUDE environment variables need to be defined',
-  );
-}
+// if (!process.env.LATITUDE || !process.env.LONGTITUDE) {
+//   throw new Error(
+//     'LATITUDE and LONGTITUDE environment variables need to be defined',
+//   );
+// }
 
-const currentLocation = {
-  latitude: parseFloat(process.env.LATITUDE!),
-  longtitude: parseFloat(process.env.LONGTITUDE!),
-};
+// const currentLocation = {
+//   latitude: parseFloat(process.env.LATITUDE!),
+//   longtitude: parseFloat(process.env.LONGTITUDE!),
+// };
 
 const now = () => new Date();
 
-const calculateSunInfo = (date: Date): SunCalcObject =>
-  suncalc.getTimes(date, currentLocation.latitude, currentLocation.longtitude);
+// const calculateSunInfo = (date: Date): SunCalcObject =>
+//   suncalc.getTimes(date, currentLocation.latitude, currentLocation.longtitude);
 
-let todaySunInfo = calculateSunInfo(now());
+// let todaySunInfo = calculateSunInfo(now());
 
 const minuteS = periodic(1000 * 60).map(now);
-const hourS = minuteS.filter(d => d.getMinutes() === 0);
-const dayS = hourS.filter(d => d.getHours() === 0);
+// const hourS = minuteS.filter(d => d.getMinutes() === 0);
+// const dayS = hourS.filter(d => d.getHours() === 0);
 
-dayS.forEach(d => {
-  todaySunInfo = calculateSunInfo(d);
-});
+// dayS.forEach(d => {
+//   todaySunInfo = calculateSunInfo(d);
+// });
 
-const sunriseS = minuteS.filter(
-  d =>
-    d.getHours() === todaySunInfo.sunrise.getHours() &&
-    d.getMinutes() === todaySunInfo.sunrise.getMinutes(),
-);
+// function enableExternalLightControl(store: Store<any>) {
+//   const externalLightGroup: Device = 'outside';
 
-const sunsetS = minuteS.filter(
-  d =>
-    d.getHours() === todaySunInfo.sunset.getHours() &&
-    d.getMinutes() === todaySunInfo.sunset.getMinutes(),
-);
+//   const sunriseS = minuteS.filter(
+//     d =>
+//       d.getHours() === todaySunInfo.sunrise.getHours() &&
+//       d.getMinutes() === todaySunInfo.sunrise.getMinutes(),
+//   );
 
-export const initializeTimeBasedActions = (store: Store<any>) => {
-  const externalLightGroup: Device = 'outside';
+//   const sunsetS = minuteS.filter(
+//     d =>
+//       d.getHours() === todaySunInfo.sunset.getHours() &&
+//       d.getMinutes() === todaySunInfo.sunset.getMinutes(),
+//   );
+
+//   // Automatically turn off outer lights
+//   sunriseS.forEach(() => {
+//     takeActions([{
+//       command: AutomationActionCommand.DISABLE_DEVICE,
+//       target: externalLightGroup,
+//       manual: false,
+//     }])
+//       .then(() => {
+//         store.dispatch(lightSet(externalLightGroup, false));
+//       })
+//       .catch(() => {
+//         error('[external-light] Unable to disable external light');
+//       });
+//   });
+
+//   // Automatically turn on outer lights
+//   sunsetS.forEach(() => {
+//     takeActions([{
+//       command: AutomationActionCommand.ENABLE_DEVICE,
+//       target: externalLightGroup,
+//       manual: false,
+//     }])
+//       .then(() => {
+//         store.dispatch(lightSet(externalLightGroup, true));
+//       })
+//       .catch(() => {
+//         error('[external-light] Unable to enable external light');
+//       });
+//   });
+// }
+
+function enableBedroomLightControl(store: Store<any>) {
+  const nightLight: Device = 'bedroom';
+
+  const sleepTimeS = minuteS.filter(
+    d => d.getHours() === 20 && d.getMinutes() === 30,
+  );
+
+  const wakeupTimeS = minuteS.filter(
+    d => d.getHours() === 7 && d.getMinutes() === 0,
+  );
 
   // Automatically turn off outer lights
-  sunriseS.forEach(() => {
-    const action: AutomationAction = {
-      command: AutomationActionCommand.DISABLE_DEVICE,
-      target: externalLightGroup,
-      manual: false,
-    };
-    takeActions([action])
+  sleepTimeS.forEach(() => {
+    takeActions([
+      {
+        command: AutomationActionCommand.ENABLE_DEVICE,
+        target: nightLight,
+        manual: false,
+      },
+    ])
       .then(() => {
-        store.dispatch(lightSet(externalLightGroup, false));
+        store.dispatch(lightSet(nightLight, false));
       })
       .catch(() => {
-        error('[external-light] Unable to disable external light');
+        error('[bedroom-light] Unable to disable bedroom light');
       });
   });
 
   // Automatically turn on outer lights
-  sunsetS.forEach(() => {
-    const action: AutomationAction = {
-      command: AutomationActionCommand.ENABLE_DEVICE,
-      target: externalLightGroup,
-      manual: false,
-    };
-    takeActions([action])
+  wakeupTimeS.forEach(() => {
+    takeActions([
+      {
+        command: AutomationActionCommand.DISABLE_DEVICE,
+        target: nightLight,
+        manual: false,
+      },
+    ])
       .then(() => {
-        store.dispatch(lightSet(externalLightGroup, true));
+        store.dispatch(lightSet(nightLight, true));
       })
       .catch(() => {
-        error('[external-light] Unable to enable external light');
+        error('[bedroom-light] Unable to enable bedroom light');
       });
   });
+}
+
+export const initializeTimeBasedActions = (store: Store<any>) => {
+  // enableExternalLightControl(store);
+  enableBedroomLightControl(store);
 };
